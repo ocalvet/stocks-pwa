3508c29caaaee5f6fa69d34d0da4c108436a6709
Ovidio Calvet <calveto@gmail.com>
2018-04-08 19:11:42 -0400

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,71 +1,214 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router';
-import { Link }  from 'react-router-dom';
-import {withStyles} from 'material-ui';
-import Button from 'material-ui/Button';
+import { withRouter } from 'react-router-dom'
+import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import logo from './logo.svg';
-import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Home from './pages/Home';
+import About from './pages/About';
+import Settings from './pages/Settings';
+
+const drawerWidth = 240;
 
 const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
+  root: {
+    flexGrow: 1,
   },
-  input: {
+  appFrame: {
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'appBarShift-left': {
+    marginLeft: drawerWidth,
+  },
+  'appBarShift-right': {
+    marginRight: drawerWidth,
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
     display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  'content-left': {
+    marginLeft: -drawerWidth,
+  },
+  'content-right': {
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: 0,
+  },
+  'contentShift-right': {
+    marginRight: 0,
   },
 });
 
-const Page = withStyles(styles)(({ title, classes }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <Button variant="raised" color="primary">
-      Hello World
-    </Button>
-    <IconButton className={classes.button} aria-label="Delete">
-        <DeleteIcon />
-      </IconButton>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-));
+class App extends React.Component {
+  state = {
+    open: false,
+  };
 
-const Home = (props) => (
-  <Page title="Home"/>
-);
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
 
-const About = (props) => (
-  <Page title="About"/>
-);
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
 
-const Settings = (props) => (
-  <Page title="Settings"/>
-);
+  navigate(route) {
+    const { history } = this.props;
+    switch (route) {
+      case 'home':
+        history.push('/');
+        break;
+      case 'about':
+        history.push('/about');
+        break;
+      case 'settings':
+        history.push('/settings');
+        break;
+      default:
+        history.push('/');
+        break;
+    }
+  }
 
-class App extends Component {
   render() {
+    const { classes, theme } = this.props;
+    const { open } = this.state;
+
+    const drawer = (
+      <Drawer
+        variant="persistent"
+        anchor={'left'}
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={this.handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button onClick={() => { this.navigate('home') }}>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button >
+            <ListItemText primary="About" onClick={() => { this.navigate('about') }} />
+          </ListItem>
+          <ListItem button >
+            <ListItemText primary="Settings" onClick={() => { this.navigate('settings') }} />
+          </ListItem>
+        </List>
+      </Drawer>
+    );
+
     return (
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/settings' component={Settings}/>
-      </Switch>
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: open,
+              [classes[`appBarShift-left`]]: open,
+            })}
+          >
+            <Toolbar disableGutters={!open}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                Offline Viewer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          {drawer}
+          <main
+            className={classNames(classes.content, classes[`content-left`], {
+              [classes.contentShift]: open,
+              [classes[`contentShift-left`]]: open,
+            })}
+          >
+            <div className={classes.drawerHeader} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/settings' component={Settings} />
+            </Switch>
+          </main>
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(withRouter(App));
