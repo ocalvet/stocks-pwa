fc6eba6bdc1684bc72ce11fcc3f12910f35b65b9
Ovidio R. Calvet <calveto@gmail.com>
2018-04-29 22:42:48 -0400

    switch symbols to use props instead of state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { loadSymbols } from '../actions'
 import Symbol from '../components/Symbol';
 import Symbols from '../providers/Symbols';
 
 class Home extends React.Component {
-  state = {
-    symbols: []
-  };
 
   constructor(props) {
     super(props);
@@ -15,11 +14,8 @@ class Home extends React.Component {
   async componentDidMount() {
     try {
       const symbols = await this.symbolsProvider.all();
-      this.setState({
-        ...this.state,
-        symbols: symbols
-      });
-      console.log('SYMBOLS', symbols);
+      console.log('PROPS SYMBOLS', symbols);
+      this.props.onSymbolsLoaded(symbols);
     } catch (e) {
       console.log('ERROR', e);
     }
@@ -34,11 +30,19 @@ class Home extends React.Component {
     return (
       <div>
         <h1>Home Page</h1>
-        {this.state.symbols.map(s => (<Symbol key={s.symbol_id} symbol={s} onSymbolSelected={(s) => this.selectSymbol(s)}/>))}
+        {this.props.symbols.map(s => (<Symbol key={s.symbol_id} symbol={s} onSymbolSelected={(s) => this.selectSymbol(s)}/>))}
         <button onClick={() => this.props.history.push('/about')}>About</button>
       </div>
     )
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  symbols: state.symbols
+});
+
+const mapActionsToProps = dispatch => ({ 
+  onSymbolsLoaded: symbols => dispatch(loadSymbols(symbols))
+});
+
+export default connect(mapStateToProps, mapActionsToProps)(Home);
