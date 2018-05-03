75b2921c56f1025c537541732702e317e5b07437
Ovidio R. Calvet <calveto@gmail.com>
2018-05-02 21:44:58 -0400

    add sample controls to the settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,15 +1,80 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import List, {
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListSubheader,
+} from 'material-ui/List';
+import Switch from 'material-ui/Switch';
+import WifiIcon from '@material-ui/icons/Wifi';
+import BluetoothIcon from '@material-ui/icons/Bluetooth';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 class Settings extends React.Component {
+  state = {
+    checked: ['wifi'],
+  };
 
-  async componentDidMount() {
-  }
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
 
   render() {
+    const { classes } = this.props;
     return (
-      <h1>Settings</h1>
+      <div>
+        <div className={classes.root}>
+          <List subheader={<ListSubheader>Settings</ListSubheader>}>
+            <ListItem>
+              <ListItemIcon>
+                <WifiIcon />
+              </ListItemIcon>
+              <ListItemText primary="Wi-Fi" />
+              <ListItemSecondaryAction>
+                <Switch
+                  onChange={this.handleToggle('wifi')}
+                  checked={this.state.checked.indexOf('wifi') !== -1}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <BluetoothIcon />
+              </ListItemIcon>
+              <ListItemText primary="Bluetooth" />
+              <ListItemSecondaryAction>
+                <Switch
+                  onChange={this.handleToggle('bluetooth')}
+                  checked={this.state.checked.indexOf('bluetooth') !== -1}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </div>
+      </div>
     );
   }
 }
 
-export default Settings;
+export default withStyles(styles)(Settings);
